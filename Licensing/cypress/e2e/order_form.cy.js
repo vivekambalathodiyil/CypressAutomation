98df417ffca4cb79e3e723ca275b9d5009a4e494
vivekambalathodiyil <56911416+vivekambalathodiyil@users.noreply.github.com>
2024-04-24 09:50:48 +0100

    24/04

--- Licensing/cypress/e2e/order_form.cy.js
+++ Licensing/cypress/e2e/order_form.cy.js
@@ -12,10 +12,18 @@ describe('Order form', ()=>{
         cy.get('#clientID').select([1])
         cy.contains('Order ID').next().type(orderNumber)
         cy.get('.input-wrapper').contains('label', 'Shared').prev('input').check()
+        cy.contains('Platform name').next().type('https://www.WDttenant.co.uk')
         cy.get('#startdate').click()
-       cy.get("#datetimepicker1").contains('9').click()
+       cy.get("#datetimepicker1").contains('1').click()
+       cy.get("#quantityContainer").type('10')
        cy.get('#endDatePicker').click()
+       cy.contains('#datetimepicker2>span.cell', '30').click({force: true})
+       document.querySelector("#datetimepicker2 > div > div:nth-child(2) > div > span:nth-child(38)")
+       
        cy.get("#datetimepicker2 > div > div:nth-child(2) > header > span.day__month_btn.up").click()
+       
+       //type('10')
+
        cy.get('.col-sm-10 > .btn').click()
 
 
---
+++ Moodle Off boarding/cypress.config.js
@@ -0,0 +1,18 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
+
+  
+module.exports = defineConfig({
+ 
+  e2e: {
+    baseUrl: 'https://elearning.etest.skillsforhealth.org.uk/',
+    
+  },
+})
